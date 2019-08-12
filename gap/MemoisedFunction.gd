953c0d9fe18c9ac53e89d6f3c3460af3302ff35f
Michael Torpey <mct25@st-andrews.ac.uk>
2019-08-12 09:33:02 +0100

    MemoisedFunction: improve doc

--- gap/MemoisedFunction.gd
+++ gap/MemoisedFunction.gd
@@ -1,20 +1,20 @@
-#
-# Memoisation: Shared persistent memoisation library for GAP and other systems
-#
-# Declarations
-#
-
 #! @Description
-#!   Default backend for storage: disk storage in the local directory 'memo'.
-BindGlobal("MEMO_DefaultCache", "file://memo/");
-
-#! @Description
-#!   Return a new function that acts the same as func, but using memoisation.
-#!   The result will be retrieved from the store if possible, or computed and
-#!   added to the store otherwise.
-#! @Arguments func
+#!   Return a special function object in the category `IsMemoisedFunction`.
+#!   This will be functionally the same as `func`, but using persistent
+#!   memoisation to store and retrieve results from a cache.
+#!
+#!   If the optional argument `opts` is specified, it should be a record which
+#!   customises how this memoisation is done.
+#!
+#! @Arguments func[, opts]
 DeclareGlobalFunction("MemoisedFunction");
 
+#! @Description
+#!   This category contains all memoised functions, special objects which wrap a
+#!   function and store previously computed results in a cache, avoiding
+#!   recomputation wherever possible.
+#!
+#!   For more information, and to create these objects, see `MemoisedFunction`.
 DeclareCategory("IsMemoisedFunction", IsFunction);
 
 #! @Description
@@ -24,5 +24,21 @@ DeclareCategory("IsMemoisedFunction", IsFunction);
 #!   true or fail
 DeclareOperation("ClearMemoisedFunction", [IsMemoisedFunction]);
 
+#! @Description
+#!   Info class for the Memoisation package.  Set this to the following levels
+#!   for different levels of information:
+#!     * 0 - No messages
+#!     * 1 - Problems only: messages describing what went wrong, with no
+#!           messages if an operation is successful
+#!     * 2 - Progress: also shows step-by-step progress of operations
+#!     * 3 - All: includes extra information such as setting attributes
+#!
+#!   Set this using, for example `SetInfoLevel(InfoMemoisation, 1)`.
+#!   Default value is 2.
 DeclareInfoClass("InfoMemoisation");
 SetInfoLevel(InfoMemoisation, 2);
+
+#! @Description
+#!   Default memoisation backend: disk storage in the local directory 'memo'.
+#!   This is the default `cache` option in `MemoisedFunction`.
+BindGlobal("MEMO_DefaultCache", "file://memo/");
